4a20dad4e8ff03da5b0a935b128f4ea8707422ac
bvsswave <tannershahan@gmail.com>
2022-03-08 19:33:24 -0700

    Updating category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,10 +48,10 @@ router.get('/:id', (req, res) => {
   ]
   })
 
-  
+
   .then(categoryData => {
   if (!categoryData) {
-    res.status(404).json({ message: "No category found with the id provided" });
+    res.status(404).json({ message: "No category with this ID provided" });
     return;
   }
 
@@ -59,21 +59,55 @@ router.get('/:id', (req, res) => {
   res.json(categoryData);
   })
   .catch(err => {
-  res.status(500).json({ message: "An error has occurred" })
+  res.status(500).json({ message: "There is an ERROR" })
   });
 
 });
 
 router.post('/', (req, res) => {
-  // create a new category
-});
+
+  // create a new categoryCategory.create({
+    
+    category_name: req.body.category_name
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    res.status(500).json({ message: "There is an ERROR"});
+  });
 
 router.put('/:id', (req, res) => {
+
   // update a category by its `id` value
+
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    res.status(500).json({ message: "There is an ERROR"});
+  });
 });
 
 router.delete('/:id', (req, res) => {
+
   // delete a category by its `id` value
+
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: "No category with this ID provided"});
+      return;
+    }
+    res.json({ message: "Category is Deleted"});
+  })
+  .catch(err => {
+    res.status(500).json({ message: "There is an ERROR" })
+    console.log(err)
+  })
 });
 
 module.exports = router;
